e6cf37f01a457b0ee691481661553489a2adc2fc
heavenburdette1988 <heavenburdette1988@gmail.com>
2022-03-02 11:46:52 -0500

    sending to test ticket 13

---
+++ TabloidMVC/Repositories/TagRepository.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+using TabloidMVC.Models;
+
+namespace TabloidMVC.Repositories
+{
+    public class TagRepository: BaseRepository,
+        ITagRepository
+    {
+        public TagRepository(IConfiguration config) : base(config) { }
+        public List<Tag> GetAllTags()
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "SELECT id, name FROM Tag";
+                    var reader = cmd.ExecuteReader();
+
+                    var Tags = new List<Tag>();
+
+                    while (reader.Read())
+                    {
+                        Tags.Add(new Tag()
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            Name = reader.GetString(reader.GetOrdinal("name")),
+                        });
+                    }
+
+                    reader.Close();
+
+                    return Tags;
+                }
+            }
+        }
+    }
+}
