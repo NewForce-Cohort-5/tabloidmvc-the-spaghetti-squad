0e7f997b6f03ced4ff52a3e51bf98ea0d1329a9f
Jaytwy24 <jtwy24@gmail.com>
2022-03-03 09:23:47 -0500

    final ticket #23

--- TabloidMVC/Models/UserType.cs
+++ TabloidMVC/Models/UserType.cs
@@ -1,15 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
-using System;
-using System.Linq;
-using System.Security.Claims;
-
-namespace TabloidMVC.Models
+﻿namespace TabloidMVC.Models
 {
     public class UserType
     {
         public int Id { get; set; }
         public string Name { get; set; }
-
-
     }
 }
